285511198c6f75959814e9f73c2d8c8b81c62369
racheldesa <racheldesa@outlook.com>
2024-03-22 16:57:40 -0500

    Game Flow logic comment

---
+++ num_guesser.py
@@ -0,0 +1,13 @@
+import random
+import math
+
+"""
+Game Flow
+----------------------------
+1. User inputs range (range min & range max)
+2. COM selects random int in range
+3. User guess
+    > user_guess == num ? Game over, display win message + num_guesses
+    > user_guess != num ? increment num_guesses, prompt for next user_guess
+
+"""
