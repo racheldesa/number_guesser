1fc30bf49ad6fa52bda865b44f996d2b1df4a1c9
racheldesa <racheldesa@outlook.com>
2024-03-22 23:39:15 -0500

    Completed while loop for main gameplay

--- num_guesser.py
+++ num_guesser.py
@@ -11,7 +11,39 @@ Game Flow
     > user_guess != num ? increment num_guesses, prompt for next user_guess
 
 """
+play_game = 'y'
 
-# Prompt user to set range 
-range_min = input("Enter range minimum: ")
-range_max = input("Enter range maximum: ")
+while play_game == 'y':
+    # Prompt user to set range 
+    range_min = input("Enter range minimum: ")
+    range_max = input("Enter range maximum: ")
+
+    # Generate random number to be guessed
+    mystery_num = random.randint(int(range_min), int(range_max))
+
+    # Keep track of number of user guesses
+    num_guesses = 0
+
+    # Game state
+    game_over = False
+
+    while not game_over:
+        user_guess = int(input("Enter guess: "))
+        num_guesses += 1
+
+        if user_guess == mystery_num:
+            game_over = True
+        elif user_guess > mystery_num: 
+            print("Too high. Try again.")
+        else:   # user_guess < mystery_num
+            print("Too low. Try again.")
+
+    # mystery_num has been guessed correctly
+    print(f"Correct! You got it in {num_guesses} tries.")
+
+    # Prompt user to play again
+    play_game = input("Play again? (y/n): ")
+    print('\n')
+
+# User has quit the game
+print("Thanks for playing!\n")
